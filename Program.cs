04afd9da54b03e1b827dd21e546089f8ddbe1b22
Andrey Rakov <coma78rav@gmail.com>
2024-02-17 00:13:33 +0300

    seminar 07

--- Program.cs
+++ Program.cs
@@ -1,1 +1,107 @@
-﻿
+﻿// Рекурсия
+// Напишите программу, которая складывает 2 числа без прямого сложения
+
+// int sumNumber(int a, int b){
+//     if (b == 0)
+//     return a;
+//     return sumNumber(a + 1, b - 1);
+// }
+// // f = sumNumber
+// f(5, 3) -> f(6, 2) -> f(7, 1) -> f(8, 0) -> 8
+// f(5, 3) <- f(6, 2) <- f(7, 1) <- f(8, 0) <- 8
+// f(5, 3) <- 8 !!! Не работает
+
+// Console.Clear();
+// Console.Write("Введите 1-е число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 2-е число: ");
+// int b = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"{a} + {b} = {sumNumber(a, b)}");
+
+// Задание 1. 
+// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+// int sumNumber(int n){
+//     // n == 0
+//     // 123 / 10 = 12    3
+//     // 12 / 10 = 1      2
+//     // 1 / 10 = 0       1
+//     if (n / 10 == 0 || n % 10 == n)
+//         return n;
+//     return sumNumber (n / 10) + n % 10;
+// }
+// f = sumNumber
+/* f(12345) -> f(1234) + 5 = 10 + 5 = 15
+                |
+                f(123) + 4 = 6 + 4 = 10
+                |
+                f(12) + 3 = 3 + 3 = 6
+                |
+                f(1) + 2 = 1 + 2 = 3
+                |
+                1
+*/
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(sumNumber(n));
+
+// Задание 2. 
+// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+
+// string printNumbers(int n){
+//     if (n == 1)
+//         return "1 ";
+    //return printNumbers(n - 1) + $"{n} "; // правильный порядок
+// f = sumNumber
+/* f(5) -> f(4) + "5 "= "1 2 3 4 5"
+             |
+             f(3) + "4 " = 
+            |
+             f(2) + "3 " = 
+            |
+            f(1) + "2 " = 
+            |
+            "1 "
+*/    
+    // return $"{n} " + printNumbers(n - 1); // обратный порядок
+// f = sumNumber
+/* f(5) -> "5 " + f(4) = "5 4 "
+                    |
+                    "4 " + f(3)  = "5 4 " + f(3)
+                            |
+                            "3 " + f(2) = "5 4 3 " + f(2)
+                                    |
+                                    "2 " + f(1) = "5 4 3 2 " + f(1)
+                                            |
+                                            "1 " = "5 4 3 2 1"
+*/
+// }
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(printNumbers(n));
+
+// Задание 3. 
+// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+
+string printConsonants(string n, int i){
+if (i == n.Length)
+return "";
+else{
+if (n[i] == 'a' || n[i] == 'e' || n[i] == 'y' || n[i] == 'u' || n[i] == 'o'
+|| n[i] == 'i')
+return printConsonants(n, i + 1);
+return $"{n[i]}" + printConsonants(n, i + 1);
+}
+}
+
+
+Console.Clear();
+Console.Write("Введите строку: ");
+string str = Console.ReadLine()!;
+Console.WriteLine(printConsonants(str, 0));
--- obj/Debug/net8.0/Seminar07.AssemblyInfo.cs
+++ obj/Debug/net8.0/Seminar07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seminar07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ed3176df073b9abf646b96bbc11bbcac15215e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seminar07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seminar07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
